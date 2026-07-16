b0971097593030f22d8e9b6eb0c8c815035c3f2d
khoinguyen26 <khoinguyen261101@gmail.com>
2026-07-16 10:37:16 +0700

    move nvim config under dotfiles repo, updated tmux, wezterm config

---
+++ home/.wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font = wezterm.font 'BlexMono Nerd Font Mono'
+config.font_size = 14
+config.color_scheme = 'tokyonight'
+
+config.enable_tab_bar= false
+config.adjust_window_size_when_changing_font_size = false
+
+-- Finally, return the configuration to wezterm:
+return config
